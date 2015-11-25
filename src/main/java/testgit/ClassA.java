8cfe00be66db74bdba6289457e5d700acf440985
Javier Garcia <javier.garcia@piksel.com>
2015-11-25 15:10:40 +0000

    segunda version

--- src/main/java/testgit/ClassA.java
+++ src/main/java/testgit/ClassA.java
@@ -3,6 +3,7 @@ package testgit;
 public class ClassA {
 
     private String field1;
+    private String field2;
 
     public String getField1() {
         return field1;
@@ -12,14 +13,25 @@ public class ClassA {
         this.field1 = field1;
     }
 
-    public ClassA(String field1) {
+    public String getField2() {
+        return field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public ClassA(String field1, String field2) {
         super();
         this.field1 = field1;
+        this.field2 = field2;
     }
 
     @Override
     public String toString() {
-        return "ClassA [field1=" + field1 + "]";
+        return "ClassA [field1=" + field1 + ", field2=" + field2 + "]";
     }
+
+
 
 }
